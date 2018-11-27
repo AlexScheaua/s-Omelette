43cdff1a9a621574aa49f881c79dae36e6dff2fb
Alex Scheaua <alex.scheaua22@gmail.com>
2018-11-27 12:58:10 +0200

    dynamic show ing+price & picture

--- src/home.js
+++ src/home.js
@@ -8,14 +8,14 @@ class Home extends Component{
     return(
       <div>
         <div id="video">
-        <video id="video" autoPlay loop muted>
-          <source src={video} type="video/mp4" />
-          Your browser does not support HTML5 video.
-        </video>
+          <video id="video" autoPlay loop muted>
+            <source src={video} type="video/mp4" />
+            Your browser does not support HTML5 video.
+          </video>
         </div>
         <div id="about">
-        <h4 className="about-title">Who are we?</h4>
-        <p className="about-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <h4 className="about-title">Who are we?</h4>
+          <p className="about-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
       </div>
     )
--- src/products.js
+++ src/products.js
@@ -7,28 +7,63 @@ import img4 from './resources/Mobile/4.png'
 
 class Products extends Component{
 
+  // ingredientsToggle = (e) => {
+  //   let selector = e.target.className[0];
+  //   let image = document.getElementsByClassName(selector)[0];
+  //   if(image.classList.contains('prodimgsactive')){
+  //     image.classList.remove('prodimgsactive')
+  //   } else {
+  //     image.classList.add('prodimgsactive')
+  //   }
+  //
+  //   let paragraph = document.getElementsByClassName(selector)[1];
+  //   if(paragraph.style.display === ''){
+  //     paragraph.style.display = "block";
+  //   } else if (paragraph.style.display === 'block'){
+  //     paragraph.style.display = "none";
+  //   } else {
+  //     paragraph.style.display = "block";
+  //   }
+  // }
+
+  active = (e) => {
+      let selector = e.target.className[0];
+      let image = document.getElementsByClassName(selector)[0];
+      let paragraph = document.getElementsByClassName(selector)[1];
+      paragraph.style.display = "block";
+      image.classList.add('prodimgsactive')
+  }
+
+  inactive = (e) => {
+      let selector = e.target.className[0];
+      let image = document.getElementsByClassName(selector)[0];
+      let paragraph = document.getElementsByClassName(selector)[1];
+      paragraph.style.display = "none";
+      image.classList.remove('prodimgsactive')
+  }
+
   render(){
     return(
       <div className="prods">
         <div className="product">
           <p className="imgTitle">Cheese & Ham</p>
-          <img src={img1} alt="" className='prodimgs' />
-          <p className="imgIngredients">Ingredients: 3 eggs, 40g Cheddar, 40g Ham <br/>Price: --</p>
+          <img src={img1} alt="" className='1 prodimgs' onMouseEnter={this.active} onMouseLeave={this.inactive} />
+          <p className="1 imgIngredients">Ingredients: 3 eggs, 40g Cheddar, 40g Ham <br/>Price: --</p>
         </div>
         <div className="product">
           <p className="imgTitle right">Classic</p>
-          <img src={img2} alt="" className='prodimgs' />
-          <p className="imgIngredients right">Ingredients: 3 eggs, bassil <br/>Price: --</p>
+          <img src={img2} alt="" className='2 prodimgs' onMouseEnter={this.active} onMouseLeave={this.inactive} />
+          <p className="2 imgIngredients right">Ingredients: 3 eggs, bassil <br/>Price: --</p>
         </div>
         <div className="product">
           <p className="imgTitle">French</p>
-          <img src={img3} alt="" className='prodimgs' />
-          <p className="imgIngredients">Ingredients: 3 eggs, green onions, butter<br/>Price: --</p>
+          <img src={img3} alt="" className='3 prodimgs' onMouseEnter={this.active} onMouseLeave={this.inactive} />
+          <p className="3 imgIngredients">Ingredients: 3 eggs, green onions, butter<br/>Price: --</p>
         </div>
         <div className="product">
           <p className="imgTitle right">Vegetables</p>
-          <img src={img4} alt="" className='prodimgs' />
-          <p className="imgIngredients right">Ingredients: 3 eggs, onion, tomatoe, mushrooms<br/>Price: --</p>
+          <img src={img4} alt="" className='4 prodimgs' onMouseEnter={this.active} onMouseLeave={this.inactive} />
+          <p className="4 imgIngredients right">Ingredients: 3 eggs, onion, tomato, mushrooms<br/>Price: --</p>
         </div>
       </div>
     )
